76daf679e105852cec5154e40b342820ab99fcec
Russell Miller <bet2buy@gmail.com>
2019-02-06 15:05:16 -0500

    finished rest manager.js

--- src/scripts/apiRestManager.js
+++ src/scripts/apiRestManager.js
@@ -10,15 +10,15 @@ fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=
     .then(rest => rest.json())
     .then(parsedRest => {
 
-        console.log(parsedRest.restaurants[0])
+        
         let restString = "";
         for(i=0; i<4; i++){
-            restName = parsedRest.restaurants[i].restaurant.name 
-            restLocation = parsedRest.restaurants[i].restaurant.location.address
+            let restName = parsedRest.restaurants[i].restaurant.name 
+            let restLocation = parsedRest.restaurants[i].restaurant.location.address
             restString += resultsBuilder(restName, restLocation, i)
         }
 
-        console.log(restName)
+        // console.log(restString)
 
 
 
