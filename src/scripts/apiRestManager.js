1d76720c80df7078ce2421012be2231ec85c0436
Russell Miller <bet2buy@gmail.com>
2019-02-06 14:52:00 -0500

    daving to merge master

--- src/scripts/apiRestManager.js
+++ src/scripts/apiRestManager.js
@@ -1,38 +1,42 @@
 
-const apiRestManager = (input) =>{
+const apiRestManager = (userInput) =>{
 
-
-
-
-
-fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=`${input}`", {
+fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=${userInput}`, {
         headers: {
             "Accept": "application/json",
             "user-key": "27a979ccc1766e311e8c4e3afe68dac7"
         }
     })
-    .then(r => r.json())
-    .then(results => {
+    .then(rest => rest.json())
+    .then(parsedRest => {
 
-        console.log(results)
-        
-        for(i=0; i<results.restaurants.length; i++){
-       
-            console.log(results.restaurants[i].restaurant.cuisines);
+        console.log(parsedRest.restaurants[0])
+        let restString = "";
+        for(i=0; i<4; i++){
+            restName = parsedRest.restaurants[i].restaurant.name 
+            restLocation = parsedRest.restaurants[i].restaurant.location.address
+            restString += resultsBuilder(restName, restLocation, i)
         }
 
-        // console.log(results.restaurants)
+        console.log(restName)
+
+
+
+        
+        
+
+        
 
         
      
     })}
+   
+    apiRestManager("burger");
+    apiRestManager("chinese");
+    apiRestManager("sandwich");
 
-    apiRestManager();
-
     
     
     
     
     
-    // https://developers.zomato.com/api/v2.1/cuisines?city_id=1138
-    // https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating
