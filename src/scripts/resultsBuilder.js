aa8049728a0fec9b445700d2ecf08a089c1aaa40
Michelle Tabor <mtabor66@outlook.com>
2019-02-07 16:25:36 -0500

    removed spaces that were causing the      /n problem

--- src/scripts/resultsBuilder.js
+++ src/scripts/resultsBuilder.js
@@ -3,7 +3,7 @@
 //You need to also pass in the iterator so that we can give the buttons original ids
 
 const resultsBuilder =(thing1, thing2, i, spanName) =>{
-    return `<p><span id ="${spanName}-${i}">${thing1}</span><span>: ${thing2}</span><button type="button" id="${spanName}-btn-${i}">save</button></p>`
+    return `<p><span id ="${spanName}-${i}">${thing1}</span><span>${thing2}</span><button type="button" id="${spanName}-btn-${i}">save</button></p>`
 
 
 }
--- src/scripts/saveItinerary.js
+++ src/scripts/saveItinerary.js
@@ -14,24 +14,30 @@ let itineraryVar = {
     restaurant: "",
     concert: ""
 }
-
-const splitStringTC =(location)=>{
-    arr = document.querySelector(location).textContent.split(": ");
-    singlestring = arr[1];
+console.log(document.querySelector("#restaurants-itin").innerText)
+const splitStringTC = (location) => {
+    let arr = document.querySelector(location).innerText.split(":")
+    console.log(arr)
+    singlestring = arr[1].trim();
     console.log(singlestring);
     return singlestring;
 }
 
 
-const changeItinerary = (event) => {
-    console.log(event)
+const changeItinerary = () => {
     let restaurantVar = splitStringTC("#restaurants-itin");
     let meetupVar = splitStringTC("#meetup-itin");
     let concertVar = splitStringTC("#concert-itin");
-    itineraryVar = {meetup: meetupVar, restauraunt: restaurantVar, concert: concertVar};
+    itineraryVar = {
+        meetup: meetupVar,
+        restauraunt: restaurantVar,
+        concert: concertVar
+    };
     console.log(itineraryVar);
     newItem(itineraryVar);
     return itineraryVar;
 }
 
-document.querySelector("#save-all-itin").addEventListener("click", changeItinerary())
+document.querySelector("#save-all-itin").addEventListener("click", () => {
+    changeItinerary()
+})
