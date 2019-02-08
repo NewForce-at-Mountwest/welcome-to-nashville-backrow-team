f55b0a43e45d5c840448ae0fd419cd95c2432f37
Michelle Tabor <mtabor66@outlook.com>
2019-02-08 15:10:19 -0500

    wrote functions to start over

--- src/scripts/startover.js
+++ src/scripts/startover.js
@@ -1,9 +1,28 @@
-const deleteALL = (itineraryObject) => {
+const deleteDB = () => {
     fetch("http://localhost:8088/itinerary/", {
         method: "DELETE", // or "POST"
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(itineraryObject)
     })
 }
+const clearFields = () => {
+    document.getElementById("restSearch").reset();
+    document.getElementById("meetSearch").reset();
+    document.getElementById("concSearch").reset();
+    document.getElementById("parkSearch").reset();
+    document.querySelector("#results-box").innerHTML = "";
+    document.getElementById("rest-itin").innerHTML = "";
+    document.getElementById("meet-itin").innerHTML = "";
+    document.getElementById("conc-itin").innerHTML = "";
+    document.getElementById("park-itin").innerHTML = "";
+}
+
+const deleteALL = () =>{
+    deleteDB();
+    clearFields();
+}
+
+document.querySelector("#start-over").addEventListener("click", () => {
+    deleteALL();
+})
