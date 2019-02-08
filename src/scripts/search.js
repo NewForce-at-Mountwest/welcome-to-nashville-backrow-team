1d338211f8f57e267e29903c28152fcfb15d42a5
Russell Miller <bet2buy@gmail.com>
2019-02-08 09:29:20 -0500

    added .replace for better search

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -19,6 +19,7 @@ document.querySelector("#search-boxes").addEventListener("click", () => {
         }
         if (targeted[0] === "park") {
             let searchTerm = document.querySelector(`#${targeted[0]}Search`).value
+            searchTerm = searchTerm.replace(" ","_")
             apiParkManager(searchTerm)
         }
     }
